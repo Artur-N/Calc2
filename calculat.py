65da8e09173679495b733e02017b15955b5fc79a
Artur <arturnl@ya.ru>
2022-02-21 03:58:49 +0300

    Add calculat

---
+++ calculat.py
@@ -0,0 +1,11 @@
+def calculation(x, y, op):
+    if op == '+':
+        res = x + y
+    if op == '-':
+        res = x - y
+    if op == '*':
+        res = x * y
+    if op == '/':
+        res = x / y
+    return res
+
